a0f2bf37d29239eea0aab2b9c345b08c78608f73
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2014-12-12 09:44:10 +0100

    add callback to detect when the api is ready

--- ggs/fate-aspects.js
+++ ggs/fate-aspects.js
@@ -1,3 +1,11 @@
+var apiReady = false;
+
+gapi.hangout.onApiReady.add(function(eventObj) {
+    console.log('Hangouts API is ready.');
+    apiReady = true;
+});
+
 $(function() {
     $('#aspects').accordion( {heightStyle: "content"} );
+
 });
