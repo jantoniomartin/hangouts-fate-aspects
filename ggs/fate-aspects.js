820a6c4cf19831128145f002b9a2fb78c09ee45a
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2014-12-12 11:15:09 +0100

    add functions to delete aspects

--- ggs/fate-aspects.js
+++ ggs/fate-aspects.js
@@ -9,6 +9,7 @@ gapi.hangout.onApiReady.add(function(eventObj) {
 
 gapi.hangout.data.onStateChanged.add(function(eventObj) {
     reloadScenario();
+    reloadScene();
 });
 
 function reloadScenario() {
@@ -30,6 +31,25 @@ function reloadScenario() {
     }
 }
 
+function reloadScene() {
+    if (apiReady) {
+        scene = JSON.parse(gapi.hangout.data.getValue('fate_scene'));
+        $('#scene').empty();
+        for (i=0; i < scene.length; i++) {
+            var l = '<li>';
+            l += scene[i];
+            l += '<span class="delete" ';
+            l += 'onClick="deleteSceneAspect(' + i + ');"';
+            l += '>&otimes;</span>';
+            l += '</li>';
+            $('#scene').append(l);
+        }
+    }
+    else {
+        console.log('Api is not ready');
+    }
+}
+
 function createScenarioAspect(name) {
     if (apiReady) {
         current = JSON.parse(gapi.hangout.data.getValue('fate_scenario'));
@@ -38,6 +58,37 @@ function createScenarioAspect(name) {
             {'fate_scenario': JSON.stringify(current)}
         );
         console.log("Added aspect: " + name);
+    }
+}
+
+function createSceneAspect(name) {
+    if (apiReady) {
+        current = JSON.parse(gapi.hangout.data.getValue('fate_scene'));
+        current[current.length] = name;
+        gapi.hangout.data.submitDelta(
+            {'fate_scene': JSON.stringify(current)}
+        );
+        console.log("Added aspect: " + name);
+    }
+}
+
+function deleteScenarioAspect(n) {
+    if (apiReady) {
+        current = JSON.parse(gapi.hangout.data.getValue('fate_scenario'));
+        current.splice(n, 1);
+        gapi.hangout.data.submitDelta(
+            {'fate_scenario': JSON.stringify(current)}
+        );
+    }
+}
+
+function deleteSceneAspect(n) {
+    if (apiReady) {
+        current = JSON.parse(gapi.hangout.data.getValue('fate_scene'));
+        current.splice(n, 1);
+        gapi.hangout.data.submitDelta(
+            {'fate_scene': JSON.stringify(current)}
+        );
     }
 }
 
@@ -52,5 +103,19 @@ $(function() {
         }
         box.val("");
         event.preventDefault();
+    });
+    
+    $('#scene_add').submit( function( event ){
+        box = $('#scene_name');
+        var t = box.val();
+        if (t != "") {
+            createSceneAspect(t);
+        }
+        box.val("");
+        event.preventDefault();
+    });
+
+    $('#container').on('click', 'span.delete', function(){
+        $(this).closest('li').remove();
     });
 });
